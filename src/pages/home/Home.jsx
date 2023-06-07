675ff6bfaeb430b8a4b76d5dd031398c31187f2d
Italo Lon kan <sanchotorres777@gmail.com>
2023-06-07 17:39:39 -0500

    Update home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/jsx-one-expression-per-line */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -19,25 +21,30 @@ const Home = () => {
         <h1 data-testid="title">DOCTORS ONLINE</h1>
         <p data-testid="subtitle">Find the best doctors in your area</p>
       </div>
-      {
-          doctors && (
-            <div className="doctorContainer" data-testid="doctorContainer">
-                {doctors.map((doctor) => (
-                  <Link to={`/detailspage/${doctor.id}`} key={doctor.id}>
-                    <div className="doctor">
-                      <img src={doctor.image} alt={`doctor${doctor.id}`} />
-                      <h3>
-                        {doctor.name}
-                        {' '}
-                        {doctor.lastname}
-                      </h3>
-                      <p>{doctor.speciality}</p>
-                    </div>
-                  </Link>
-                ))}
-            </div>
-          )
-        }
+      {doctors && (
+        <div
+          className="doctorContainer"
+          data-testid="doctorContainer"
+        >
+          {doctors.map((doctor) => (
+            <Link
+              to={`/detailspage/${doctor._id}`}
+              key={doctor._id}
+            >
+              <div className="doctor">
+                <img
+                  src={doctor.image}
+                  alt={`doctor${doctor._id}`}
+                />
+                <h3>
+                  {doctor.name.toUpperCase()} {doctor.lastname.toUpperCase()}
+                </h3>
+                <p>{doctor.speciality}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
